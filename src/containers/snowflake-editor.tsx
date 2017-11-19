ca388f0b903fbbff7aadc641875df7014d0591b8
mpearson <supermacaroni@gmail.com>
2017-11-19 00:28:05 -0800

    added input for symmetry number added orbit controls

--- src/containers/snowflake-editor.tsx
+++ src/containers/snowflake-editor.tsx
@@ -3,9 +3,12 @@ import * as React from 'react';
 // import { PageSection } from '../../components/page-section';
 // import { PageHero } from '../../components/page-hero';
 import * as THREE from "three";
+const OrbitControls = require("three-orbitcontrols");
+
 import * as _ from "lodash";
 
 import {ProceduralGeometry2D} from "../models/procedural2D";
+import { ChangeEvent } from 'react';
 
 // import Snowflake
 
@@ -14,90 +17,77 @@ export interface SnowflakeEditorProps {
 
 }
 
-export class SnowflakeEditor extends React.Component<SnowflakeEditorProps> {
+export interface SnowflakeEditorState {
+  symmetry: number;
+  size: number;
+}
+
+
+
+export class SnowflakeEditor extends React.Component<SnowflakeEditorProps, SnowflakeEditorState> {
   private container: HTMLDivElement;
   private updateTimer: number;
   private updateInterval = 20;
+  private symmetryInput: HTMLInputElement;
+  // private symmetry: number;
 
   // camera and rendering
   private width: number;
   private height: number;
-  private renderer: THREE.Renderer;
+  private renderer: THREE.WebGLRenderer;
   private scene: THREE.Scene;
   private camera: THREE.Camera;
+  private controls: THREE.OrbitControls;
+  // private light: THREE.PointLight;
 
   private snowflake: ProceduralGeometry2D;
 
-  // private count: number;
-  // private shapeSize: number;
-  // private points: THREE.Vector3[];
-  // private targets: THREE.Vector3[];
-  // private geometry: THREE.Geometry;
-  // private vertexDots: THREE.Points;
-  // private material: THREE.LineBasicMaterial;
-  // private outline: THREE.LineLoop;
-
   constructor(props: SnowflakeEditorProps) {
     super(props);
+    this.state = {
+      symmetry: 6,
+      size: 200,
+    }
 
     this.width = 800;
     this.height = 600;
 
     // scene
-    const scene = new THREE.Scene();
-    scene.background = new THREE.Color(0x234674);
+    this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x234674);
 
     // camera
-    const camera = new THREE.PerspectiveCamera(50, this.width / this.height, 1, 1000);
-    camera.position.set(0, 0, 500);
-    scene.add(camera);
+    this.camera = new THREE.PerspectiveCamera(50, this.width / this.height, 1, 1000);
+    this.camera.position.set(0, 0, 500);
+    this.scene.add(this.camera);
 
     // lighting
-    // const light = new THREE.PointLight(0xffffff, 0.8);
-    // camera.add(light);
+    // this.light = new THREE.PointLight(0xffffff, 0.8);
+    // this.camera.add(this.light);
 
     // renderer
-    const renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio(window.devicePixelRatio);
-    renderer.setSize(this.width, this.height);
+    this.renderer = new THREE.WebGLRenderer();
+    this.renderer.setPixelRatio(window.devicePixelRatio);
+    this.renderer.setSize(this.width, this.height);
 
-    this.camera = camera;
-    this.scene = scene;
-    this.renderer = renderer;
-
-
-    // this.count = 0;
-    // this.points = [];
-    // this.targets = [];
-
-    // this.material = new THREE.LineBasicMaterial({
-    //   color: 0xffffff,
-    //   opacity: 0.5,
-    //   alphaTest: 0.4,
-    //   transparent: true,
-    // });
-    // this.shapeSize = 200;
-    this.snowflake = this.nucleate(6, 200);
+    // controls
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.enableRotate = true;
+    this.controls.enablePan = false;
+    this.controls.enableZoom = true;
+    this.controls.minDistance = 400;
+    this.controls.maxDistance = 600;
+    this.controls.minAzimuthAngle = Math.PI * -0.35;
+    this.controls.maxAzimuthAngle = Math.PI * 0.35;
+    this.controls.minPolarAngle = Math.PI * 0.15;
+    this.controls.maxPolarAngle = Math.PI * 0.85;
+    this.controls.rotateSpeed = 0.25;
+    this.controls.dampingFactor = 0.2;
 
 
-    // const sphere = new THREE.Mesh(
-    //   new THREE.SphereGeometry(50, 32, 32), new THREE.MeshBasicMaterial({color: 0xffffff}));
-
-    // // Move the Sphere back in Z so we
-    // // can see it.
-    // sphere.position.z = -300;
-
-    // // Finally, add the sphere to the scene.
-    // scene.add(sphere);
-
-
-    // for (var i = 0; i < this.count; i++) {
-    //   this.points.push(new THREE.Vector3(0, 0, 0));
-    //   this.targets.push(new THREE.Vector3(0, 0, 0));
-    // }
-
-
-    // this.outline = new THREE.Mesh(this.geometry, this.material);
+    // action!
+    this.regenerate();
 
     this.regenerate = this.regenerate.bind(this);
     // this.updateSimulation = this.updateSimulation.bind(this);
@@ -106,10 +96,8 @@ export class SnowflakeEditor extends React.Component<SnowflakeEditorProps> {
   }
 
   private renderFrame () {
-    // Draw!
+    this.controls.update();
     this.renderer.render(this.scene, this.camera);
-
-    // Schedule the next frame.
     requestAnimationFrame(this.renderFrame);
   }
 
@@ -122,43 +110,55 @@ export class SnowflakeEditor extends React.Component<SnowflakeEditorProps> {
     this.updateTimer = null;
   }
 
+  public componentDidUpdate() {
+    this.regenerate();
+  }
+
   public render() {
+    const { symmetry, size } = this.state;
+
     return (
       <div>
         <button onClick={this.regenerate}>Regenerate</button>
+        <input
+          type="range"
+          min="3"
+          max="12"
+          value={symmetry}
+          onChange={event => {
+            this.setState({ symmetry: parseInt(event.target.value, 10) });
+            // this.regenerate();
+          }}
+        />
+        <span>{symmetry}</span>
         <div id="webgl-wrapper" ref={elem => {
-          this.container = elem;
-          elem.appendChild(this.renderer.domElement);
+          if (elem !== null) {
+            this.container = elem;
+            elem.appendChild(this.renderer.domElement);
+          }
         }}></div>
       </div>
     );
   }
 
-
-    // this.scene.add(this.vertexDots);
-
   private nucleate(sides: number, shapeSize: number): ProceduralGeometry2D {
-
     const snowflake = new ProceduralGeometry2D({
       vertexCount: 9000,
-      verticesUsed: 6,
+      verticesUsed: sides,
     });
-
 
     const verts = snowflake.vertices;
 
     const angleStep = Math.PI * 2 / sides;
 
-    for (let i = 0, xIndex = 0, yIndex = 1, zIndex = 2; i < sides; i++, xIndex += 3, yIndex += 3, zIndex += 3) {
-
-
+    for (let i = 0, xIndex = 0; i < sides; i++, xIndex += 3) {
       // sin and cos swapped because we want it to start at the top
       const x = Math.sin(angleStep * i) * shapeSize * 0.5;
       const y = Math.cos(angleStep * i) * shapeSize * 0.5;
 
       verts[xIndex] = x;
-      verts[yIndex] = y;
-      verts[zIndex] = 0;
+      verts[xIndex + 1] = y;
+      verts[xIndex + 2] = 0;
     }
 
     // this.createGeometry();
@@ -207,23 +207,16 @@ export class SnowflakeEditor extends React.Component<SnowflakeEditorProps> {
   }
  */
   private regenerate() {
-    // this.nucleate(Math.round(3 + Math.random() * 9));
-    let { scene, snowflake } = this;
-    if (snowflake) {
-      scene.remove(snowflake.outline);
-      scene.remove(snowflake.vertexDots);
+    const { scene } = this;
+    if (this.snowflake) {
+      scene.remove(this.snowflake.outline);
+      scene.remove(this.snowflake.vertexDots);
     }
 
-    this.snowflake = snowflake = this.nucleate(6, 200);
-    scene.add(snowflake.outline);
-    scene.add(snowflake.vertexDots);
-    // for (var i = 0; i < this.count; i++) {
-    //   var x = Math.random() * this.shapeSize - (0.5 * this.shapeSize);
-    //   var y = Math.random() * this.shapeSize - (0.5 * this.shapeSize);
-
-    //   this.targets[i].x = x;
-    //   this.targets[i].y = y;
-    // }
+    this.snowflake = this.nucleate(this.state.symmetry, this.state.size);
+    // this.snowflake = this.nucleate(Math.round(3 + Math.random() * 9), 50 + Math.random() * 250);
+    scene.add(this.snowflake.outline);
+    scene.add(this.snowflake.vertexDots);
   }
 
 }
